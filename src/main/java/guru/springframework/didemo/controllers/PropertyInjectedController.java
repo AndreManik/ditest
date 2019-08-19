111ec13b543705e2d63caa8dba4372b747065309
Manik Andrey <andre.ivan.manik@gmail.com>
2019-08-19 12:37:40 +0300

    SFW5-36 | Using Spring Qualifier Annotations

--- src/main/java/guru/springframework/didemo/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/didemo/controllers/PropertyInjectedController.java
@@ -2,16 +2,17 @@ package guru.springframework.didemo.controllers;
 
 import guru.springframework.didemo.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
 
     @Autowired
-    public GreetingService greetingService;
+    public GreetingService greetingServiceImpl;
 
     public String sayHello() {
-        return greetingService.sayGreeting();
+        return greetingServiceImpl.sayGreeting();
     }
 
 
--- src/main/java/guru/springframework/didemo/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/didemo/controllers/SetterInjectedController.java
@@ -2,6 +2,7 @@ package guru.springframework.didemo.controllers;
 
 import guru.springframework.didemo.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -12,6 +13,7 @@ public class SetterInjectedController {
         return greetingService.sayGreeting();
     }
     @Autowired
+    @Qualifier("setterGreetingService")
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
