c03451e234ad1031bdac12185c05a82122892d05
Manik Andrey <andre.ivan.manik@gmail.com>
2019-08-19 14:39:25 +0300

    SFW5-36 | Spring Profiles

--- src/main/java/guru/springframework/didemo/services/PrimaryRussianGreetingService.java
+++ src/main/java/guru/springframework/didemo/services/PrimaryRussianGreetingService.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("ru")
 @Primary
-@Profile("en")
-public class PrimaryGreetingService implements GreetingService{
+public class PrimaryRussianGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greeting service";
+        return "Приветственный примари сервис";
     }
 }
