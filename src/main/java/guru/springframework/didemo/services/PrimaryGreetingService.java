9c9eab5227994abbdd285214d25b3e0e2989c94b
Manik Andrey <andre.ivan.manik@gmail.com>
2019-08-19 14:29:44 +0300

    SFW5-36 | Primary Annotation for Spring Beans

--- src/main/java/guru/springframework/didemo/services/PrimaryGreetingService.java
+++ src/main/java/guru/springframework/didemo/services/PrimaryGreetingService.java
@@ -1,4 +1,13 @@
 package guru.springframework.didemo.services;
 
-public class PrimaryGreetingService {
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+public class PrimaryGreetingService implements GreetingService{
+    @Override
+    public String sayGreeting() {
+        return "Hello - Primary Greeting service";
+    }
 }
