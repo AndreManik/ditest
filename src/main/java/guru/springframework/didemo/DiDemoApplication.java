c0590d27804b77f269c8656cb6ca416a3b00f88d
Andre <andre.ivan.manik@gmail.com>
2019-08-23 17:04:26 +0300

    use external properties

--- src/main/java/guru/springframework/didemo/DiDemoApplication.java
+++ src/main/java/guru/springframework/didemo/DiDemoApplication.java
@@ -4,6 +4,7 @@ import guru.springframework.didemo.controllers.ConstructorInjectedController;
 import guru.springframework.didemo.controllers.MyController;
 import guru.springframework.didemo.controllers.PropertyInjectedController;
 import guru.springframework.didemo.controllers.SetterInjectedController;
+import guru.springframework.didemo.examplebean.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,10 +17,16 @@ public class DiDemoApplication {
 
         MyController controller = (MyController) ctx.getBean("myController");
 
-        System.out.println(controller.hello());
+        /*System.out.println(controller.hello());
         System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
         System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());*/
+
+        FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
+
+        System.out.println("  name: " + fakeDataSource.getUser() + ",\n" +
+                           "  pass: " + fakeDataSource.getPassword() + ", \n" +
+                           "  url:  " + fakeDataSource.getUrl());
     }
 
 }
